3d2aa19197da5ff060f343da172539c354770d29
NxPKG <116948796+NxPKG@users.noreply.github.com>
2024-04-23 22:20:46 +0600

    Create 01-has-all-tools.sh

---
+++ images/sdk/tests/01-has-all-tools.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+set -o errexit -o nounset -o errtrace -o pipefail
+
+IMAGE_NAME=${IMAGE_NAME:-"unset"}
+if [[ "${IMAGE_NAME}" == "unset" ]]; then
+    echo "Must set IMAGE_NAME in the environment. Exiting."
+    exit 1
+fi
+
+docker run --rm --entrypoint bash "${IMAGE_NAME}" -xc \
+    'which goimports &&
+     tree --version &&
+     go version &&
+     make --version &&
+     curl --version &&
+     git version &&
+     apk --version &&
+     bwrap --version &&
+     melange version &&
+     apko version &&
+     wolfictl version'
+
+# Check that everything is up to date
+# Freshly minted image, should be fully up to date, this however will
+# catch any package renames/provides that apko resolved using out of
+# date packages, due to packaging mistakes or ongoing transitions
+docker run --rm --entrypoint bash "${IMAGE_NAME}" -xc \
+     'apk update; apk upgrade --all --latest || true'
